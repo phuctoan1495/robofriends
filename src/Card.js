bbadc7a07ee737739bc92727a4897f851ce91392
Phuc Toan <phuctoan1495@gmail.com>
2020-06-09 20:05:13 +0700

    remove unuseable js and add robots.js

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () =>{
+	return(
+		<div>
+			<img className='tc' alt='photo' src='https://robohash.org/example1?200x200' />
+			<div className='tc' >
+				<h2>Phillip Dang</h2>
+				<p>phillipDang@gmail.com</p>
+			</div>
+		</div>
+	);
+}
+
+export default Card;
