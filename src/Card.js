c9d5ef37f7525b64957280261c942c73c51df88e
Phuc Toan <phuctoan1495@gmail.com>
2020-06-09 20:41:06 +0700

    make cards more flexible

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) =>{
 	return(
 		<div className ='bg-light-green dib br3 ma2 pa3 grow' >
-			<img alt='photo' src='https://robohash.org/example1?200x200' />
+			<img alt='photo' src= {'https://robohash.org/'+ props.id + '?200x200'} />
 			<div className='tc'>
 				<h2>{props.name}</h2>
 				<p>{props.email}</p>
