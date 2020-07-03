d653ca5dc589dfd1fa7ba12bfe8166774d6f3b8d
Phuc Toan <phuctoan1495@gmail.com>
2020-07-03 20:43:04 +0700

    make robots a scollable box

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 // import { robots } from './robots';
 
 class App extends React.Component {
@@ -38,7 +39,9 @@ class App extends React.Component {
 				<div className="tc">
 					<h1>RoboFriends</h1>
 					<SearchBox searchChange={ this.onSearchChange }/>
-				 	<CardList robots={filterRobot} />
+					<Scroll>
+				 		<CardList robots={filterRobot} />
+				 	</Scroll>
 				</div>
 			);
 		}
