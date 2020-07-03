9ceb5029519af76ab639bc7d520651c60d66877c
Phuc Toan <phuctoan1495@gmail.com>
2020-07-03 20:21:52 +0700

    Make app more realistic by using information from internet instead of a file, using lifecycle of component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+// import { robots } from './robots';
 
 class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchField: ''
 		};
 	};
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({robots: users}));
+	}
 
 	onSearchChange = (event) => {
 		this.setState({ searchField: event.target.value });
@@ -25,13 +31,17 @@ class App extends React.Component {
 			return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
 		});
 
-		return (
-			<div className="tc">
-				<h1>RoboFriends</h1>
-				<SearchBox searchChange={ this.onSearchChange }/>
-			 	<CardList robots={filterRobot} />
-			</div>
-		);
+		if (this.state.robots.length === 0){
+			return <h1 className="tc">Loading</h1>
+		}else{
+			return (
+				<div className="tc">
+					<h1>RoboFriends</h1>
+					<SearchBox searchChange={ this.onSearchChange }/>
+				 	<CardList robots={filterRobot} />
+				</div>
+			);
+		}
 	};
 }
 
