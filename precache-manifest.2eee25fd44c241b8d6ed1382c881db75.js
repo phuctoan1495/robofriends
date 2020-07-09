5be2b46ef78855a9712387a5f7450c782690183d
Phuc Toan <phuctoan1495@gmail.com>
2020-07-09 20:27:25 +0700

    Updates

---
+++ precache-manifest.2eee25fd44c241b8d6ed1382c881db75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45f6fa40db6a3769b482b0ad67dae3e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0a6515bb254948fabc14",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fc47502a24af310c5976",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0a6515bb254948fabc14",
+    "url": "/robofriends/static/js/2.269c1c0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.269c1c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc47502a24af310c5976",
+    "url": "/robofriends/static/js/main.2ec7f673.chunk.js"
+  },
+  {
+    "revision": "dd639abf78a70a829f07",
+    "url": "/robofriends/static/js/runtime-main.191a4bb8.js"
+  }
+]);
